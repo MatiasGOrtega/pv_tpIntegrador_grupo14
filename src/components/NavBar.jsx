6c9d1267a55aaf9e38dc99fa7b078076d8f3e3ba
MatiasGOrtega <mat-1998-ort@hotmail.com>
2025-06-23 18:19:47 -0300

    Refactor Navbar to use TabNav component for improved styling and structure

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,32 @@
-import { Link } from 'react-router-dom';
+import { TabNav, Text } from '@radix-ui/themes';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav style={styles.nav}>
-      <Link to="/">Home</Link>
-      <Link to="/favorites">Favoritos</Link>
-      <Link to="/product/new">Crear producto</Link>
-    </nav>
+    <>
+      <TabNav.Root size="2" color="indigo" className='bg-white shadow-md' justify={'center'} >
+        <TabNav.Link asChild active >
+          <NavLink to="/" >
+            <Text size="5" weight="bold" color="indigo">Home</Text>
+          </NavLink>
+        </TabNav.Link>
+        <TabNav.Link asChild active>
+          <NavLink to="/products/add" >
+            <Text size="5" weight="bold" color="indigo">Crear Producto</Text>
+          </NavLink>
+        </TabNav.Link>
+        <TabNav.Link asChild active>
+          <NavLink to="/favorites" >
+            <Text size="5" weight="bold" color="indigo">Favoritos</Text>
+          </NavLink>
+        </TabNav.Link>
+        <TabNav.Link asChild active>
+          <NavLink to="/about">
+            <Text size="5" weight="bold" color="indigo">Acerca De</Text>
+          </NavLink>
+        </TabNav.Link>
+      </TabNav.Root>
+    </>
   );
 };
 
