8bc30fcee39900a96a606ce6942fbe255f76356f
MatiasGOrtega <mat-1998-ort@hotmail.com>
2025-06-29 17:43:26 -0300

    Add auth reducer to Redux store for user authentication management

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productsReducer from './productsSlice';
+import usersReducer from './userSlice';
 
 export const store = configureStore({
   reducer: {
     products: productsReducer,
+    auth: usersReducer,
   },
 });
